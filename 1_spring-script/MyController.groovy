553cd54803e788b4a6e0d2987fb2d122662690f7
David Dawson <david.dawson@simplicityitself.com>
2014-11-19 10:30:57 +0000

    Stuff

--- 1_spring-script/MyController.groovy
+++ 1_spring-script/MyController.groovy
@@ -7,5 +7,12 @@ class MyController {
     String home() {
         return "Hello World!"
     }
+    
+    @RequestMapping("/jsondata")
+    //note auto generation of JSON response type
+    @ResponseBody
+    Map jsondata() {
+        return [myData:"Is In JSON Format"]
+    }
 }
 
